f14950e36384364cebcf4b7f6fecea9bfbc6793e
Charly <charly81.zirpoli@gmail.com>
2022-08-03 19:09:47 -0300

    mi first commit

---
+++ account.common/src/main/java/com/techbank/account/common/events/FundsWithdrawnEvent.java
@@ -0,0 +1,16 @@
+package com.techbank.account.common.events;
+
+import com.techbank.cqrs.core.events.BaseEvent;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//@SuperBuilder
+public class FundsWithdrawnEvent extends BaseEvent {
+	private double amount;
+
+}
